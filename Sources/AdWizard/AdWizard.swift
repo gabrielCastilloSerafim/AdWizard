1d352fc72a73d3fa26ba5bfab79874f03ac48459
Gabriel Castillo <sk8.andrade@gmail.com>
2024-01-14 10:50:43 +0100

    first commit

--- Sources/AdWizard/AdWizard.swift
+++ Sources/AdWizard/AdWizard.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public final class AdWizard {
+    
+    public init() {
+        
+    }
+    
+    public func printSome() {
+        print("Some")
+    }
+}
